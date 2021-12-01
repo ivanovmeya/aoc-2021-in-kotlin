44e54bd69dd53d200409aafc81abb272b37cce15
Maksim Ivanov <maksim@treamer.com>
2021-12-01 12:46:11 +0300

    day one solution

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,8 @@ tasks {
         gradleVersion = "7.3"
     }
 }
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.6.0")
+    testImplementation("junit:junit:4.12")
+}
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,51 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+
+        var numberOfIncreases = 0
+        for (i in 1 until depths.size) {
+            if (depths[i - 1] < depths[i]) {
+                numberOfIncreases++
+            }
+        }
+        return numberOfIncreases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val slidingWindowSize = 3
+        //not enough data to create sliding window of 3
+        if (input.size < slidingWindowSize) return 0
+
+        var numberOfIncreases = 0
+        val depth = input.map { it.toInt() }
+
+        var windowStart = 0
+        var windowEnd = slidingWindowSize-1
+        var previousSlidingWindowSum = depth.subList(windowStart, windowEnd).sum()
+
+        while(windowEnd < depth.size-1) {
+            windowStart++
+            windowEnd++
+            val currentSum = previousSlidingWindowSum - depth[windowStart-1] + depth[windowEnd]
+            if (currentSum > previousSlidingWindowSum) numberOfIncreases++
+            previousSlidingWindowSum = currentSum
+        }
+        return numberOfIncreases
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val expectedPart1 = 7
+    val actualPart1 = part1(testInput)
+    println("test data, part1 = $actualPart1")
+    check(part1(testInput) == expectedPart1)
+
+    val expectedPart2 = 5
+    val actualPart2 = part2(testInput)
+    println("test data, part2 = $actualPart2")
+    check(actualPart2 == expectedPart2)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("real data: part1 = ${part1(input)}")
+    println("real data: part2 = ${part2(input)}")
 }
